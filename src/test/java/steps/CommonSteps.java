1bde62a13b8eadbb4e7431bf559098d67b324145
Orlando Avila <orlando.avilag@hotmail.com>
2023-12-07 13:40:37 -0600

    Extent report implementation.

--- src/test/java/steps/CommonSteps.java
+++ src/test/java/steps/CommonSteps.java
@@ -11,21 +11,20 @@ import utils.TakeScreenShot;
 
 public class CommonSteps
 {
-    private final LoginPage loginPage1 = new LoginPage(SetUp.getInstance().getDrivers().get(0));
-    private final LoginPage loginPage2 = new LoginPage(SetUp.getInstance().getDrivers().get(1));
-    TakeScreenShot screenShot1 = new TakeScreenShot(SetUp.getInstance().getDrivers().get(0));
-    TakeScreenShot screenShot2 = new TakeScreenShot(SetUp.getInstance().getDrivers().get(1));
-
     @Given("user opens the app")
     public void validateAppOpen() {
-        Assert.assertTrue(loginPage1.homeButtonDisplayed());
-        Assert.assertTrue(loginPage2.homeButtonDisplayed());
+        for (int i = 0; i < SetUp.getInstance().getDrivers().size(); i++) {
+            LoginPage loginPage = new LoginPage(SetUp.getInstance().getDrivers().get(i));
+            Assert.assertTrue(loginPage.homeButtonDisplayed());
+        }
     }
 
     @AfterStep
     public void screenShot(Scenario scenario) {
-        screenShot1.takeScreenShot(scenario);
-        screenShot2.takeScreenShot(scenario);
+        for (int i = 0; i < SetUp.getInstance().getDrivers().size(); i++) {
+            TakeScreenShot takeScreenShot = new TakeScreenShot(SetUp.getInstance().getDrivers().get(i));
+            takeScreenShot.takeScreenShot(scenario);
+        }
     }
 
     @After
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -5,34 +5,35 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import pages.LoginPage;
-import utils.Retry;
 import utils.SetUp;
 
 public class LoginSteps
 {
-    private final LoginPage loginPage1 = new LoginPage(SetUp.getInstance().getDrivers().get(0));
-    private final LoginPage loginPage2 = new LoginPage(SetUp.getInstance().getDrivers().get(1));;
-
-
     @When("entering email (.*) and password (.*)$")
     public void setUserCredentials(String email, String password)
     {
-        loginPage1.writeCredentials(email, password);
-        loginPage2.writeCredentials(email, password);
+        for (int i = 0; i < SetUp.getInstance().getDrivers().size(); i++) {
+            LoginPage loginPage = new LoginPage(SetUp.getInstance().getDrivers().get(i));
+            loginPage.writeCredentials(email, password);
+        }
     }
 
     @And("press login button")
     public void pressLoginButton()
     {
-        loginPage1.pressLoginButton();
-        loginPage2.pressLoginButton();
+        for (int i = 0; i < SetUp.getInstance().getDrivers().size(); i++) {
+            LoginPage loginPage = new LoginPage(SetUp.getInstance().getDrivers().get(i));
+            loginPage.pressLoginButton();
+        }
     }
 
     @Then("verify user successfully logged in")
     public void successfulLogin()
     {
-        Assert.assertEquals("You are logged in!", loginPage1.getLoginModalText());
-        Assert.assertEquals("You are logged in!", loginPage2.getLoginModalText());
+        for (int i = 0; i < SetUp.getInstance().getDrivers().size(); i++) {
+            LoginPage loginPage = new LoginPage(SetUp.getInstance().getDrivers().get(i));
+            Assert.assertEquals("You are logged in!", loginPage.getLoginModalText());
+        }
     }
 
 /*    public void invalidLogin()
